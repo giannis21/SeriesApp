14bc437fb5ebd68b66b399c7cd0f37bcde0dbfbf
giannis <giannisfragoulis21@gmail.com>
2021-09-12 20:15:25 +0300

    glide in seperate function

--- app/src/main/java/com/example/seriesapp/paging/BindingAdapter.kt
+++ app/src/main/java/com/example/seriesapp/paging/BindingAdapter.kt
@@ -20,6 +20,7 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.example.seriesapp.R
 import com.example.seriesapp.data.characters.Result
+import com.example.seriesapp.utils.loadImageGeneric
 import com.example.seriesapp.utils.px
 
 
@@ -27,44 +28,10 @@ object BindingAdapter  {
 
     @BindingAdapter("imageUrl", "format")
     @JvmStatic
-    fun <T> loadImage(view: ImageView, character: T, format: String) {
-
-        try {
-            val url :String
-            val extension:String
-
-            (character as Result).apply {        //i pass T type object so to be generic and i cast it to Result in order to use its attributes
-                url = if(!thumbnail.path.contains("https"))
-                    thumbnail.path.replace("http", "https")
-                else
-                    thumbnail.path
-
-                extension=thumbnail.extension
-            }
-
-
-            val completeUrl = "$url/$format.$extension"
-            val progressBar: ProgressBar? = (view.parent as? ConstraintLayout)?.findViewById<ProgressBar>(R.id.progressBar)
-
-            Glide.with(view.context).load(completeUrl)
-                    .apply(RequestOptions().transform(FitCenter(), GranularRoundedCorners(16.px.toFloat(), 0.toFloat(), 0.toFloat(), 16.px.toFloat())))
-                    .skipMemoryCache(true)
-                    .listener(object : RequestListener<Drawable> { //this listener telling me when the load finished succesfully or not, so i have to hide progressBar
-                        override fun onLoadFailed(e: GlideException?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                            progressBar?.visibility = View.GONE
-                            return false
-                        }
-
-                        override fun onResourceReady(resource: Drawable?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                            progressBar?.visibility = View.GONE
-                            return false
-                        }
-
-                    })
-                    .into(view)
-
-        }catch (e: Exception){
-            println(e.message)
+    fun <T> loadImage(view: ImageView, character: Result, format: String) {
+        val progressBar: ProgressBar? = (view.parent as? ConstraintLayout)?.findViewById<ProgressBar>(R.id.progressBar)
+        loadImageGeneric(view, character, format){
+            progressBar?.visibility=View.GONE
         }
     }
 
--- app/src/main/java/com/example/seriesapp/utils/ext.kt
+++ app/src/main/java/com/example/seriesapp/utils/ext.kt
@@ -1,6 +1,21 @@
 package com.example.seriesapp.utils
 
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.media.Image
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.example.seriesapp.R
+import com.example.seriesapp.data.characters.Result
 import java.nio.charset.StandardCharsets.UTF_8
 import java.security.MessageDigest
 
@@ -22,4 +37,47 @@ fun ByteArray.toHex(): Map<String, String> {
 fun getStr():String{
     currentTimeStamp=System.currentTimeMillis().toString()
     return currentTimeStamp + Constants.PRIVATE_KEY + Constants.API_KEY
+}
+
+fun <M,T,H> loadImageGeneric(view: M, character: T, format: H, imageLoadedListener: () ->Unit){
+    try {
+        var url :String=""
+        var extension:String=""
+        val genericView:View = view as ImageView
+
+        if(character is Result){
+            (character as Result).apply {        //i pass T type object so to be generic and i cast it to Result in order to use its attributes
+                url = if(!thumbnail.path.contains("https"))
+                    thumbnail.path.replace("http", "https")
+                else
+                    thumbnail.path
+
+                extension=thumbnail.extension
+            }
+        }
+
+
+
+        val completeUrl = "$url/${format.toString()}.$extension"
+
+        Glide.with(genericView.context).load(completeUrl)
+            .apply(RequestOptions().transform(FitCenter(), GranularRoundedCorners(16.px.toFloat(), 0.toFloat(), 0.toFloat(), 16.px.toFloat())))
+            .skipMemoryCache(true)
+            .listener(object : RequestListener<Drawable> { //this listener telling me when the load finished succesfully or not, so i have to hide progressBar
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    imageLoadedListener.invoke()
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    imageLoadedListener.invoke()
+                    return false
+                }
+
+            })
+            .into(view)
+
+    }catch (e: Exception){
+        println(e.message)
+    }
 }
